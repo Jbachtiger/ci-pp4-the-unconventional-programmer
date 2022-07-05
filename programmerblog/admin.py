effdd795774b5415177eb0e0cf7921ddf2460ec6
Jbachtiger <jbachtiger@gmail.com>
2022-07-05 14:30:08 +0000

    Add post model, superuser and admin functionality for posts

--- programmerblog/admin.py
+++ programmerblog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+#Admin page functionality for posts
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'publish_date')
+    list_filter = ('publish_date', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'body']
--- programmerblog/models.py
+++ programmerblog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+# Post model to display post details when a post is created
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    title_tag = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, default="blog-post")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    hero_image = CloudinaryField('image', default='placeholder')
+    publish_date = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    body = models.TextField()
+    topic = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['-publish_date']
+
+    def __str__(self):
+        return self.title + '|' + str(self.author)
+    
+    def number_of_likes(self):
+        return self.likes.count()
+
+
