9f892527f50835dabe02c101ad8357098eab0fd2
Jbachtiger <jbachtiger@gmail.com>
2022-07-06 14:10:31 +0000

    Add post_details html page and link it up with home html so it can be accessed

--- programmerblog/urls.py
+++ programmerblog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home, PostDetail
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
+    path('posts/<int:pk>', PostDetail.as_view(), name='post-detail'),
 ]
--- programmerblog/views.py
+++ programmerblog/views.py
@@ -6,4 +6,9 @@ from .models import Post
 class Home(generic.ListView):
     model = Post
     template_name = 'home.html'
-    
+    paginate_by = 20
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_details.html'
