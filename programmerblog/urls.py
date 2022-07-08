a7f67a1636e50049583962beb7eadb922ffad67a
Jbachtiger <jbachtiger@gmail.com>
2022-07-08 15:19:14 +0000

    Add edit-post url

--- programmerblog/urls.py
+++ programmerblog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Home, PostDetail, CreatePost
+from .views import Home, PostDetail, CreatePost, EditPost
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
     path('post/<slug:slug>', PostDetail.as_view(), name='post-detail'),
     path('create-post/', CreatePost.as_view(), name='create-post'),
+    path('post/edit/<slug:slug>', EditPost.as_view(), name='edit-post'),
 ]
