6483d31eeea19e4d4a7d0fdd84cda28c7f4b6b4d
Jbachtiger <jbachtiger@gmail.com>
2022-07-12 14:28:35 +0000

    Fix migrations error by reattaching a new database

--- programmerblog/models.py
+++ programmerblog/models.py
@@ -40,16 +40,10 @@ class Post(models.Model):
 
 
 class Comment (models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    name = models.CharField(max_length=255)
-    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     publish_date = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
-
-
-    class Meta:
-        ordering = ['-publish_date']
-
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    body = models.TextField()
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return self.user.username
