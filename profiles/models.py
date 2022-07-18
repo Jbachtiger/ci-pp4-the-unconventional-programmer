fc09e78ac08558047c456e8ba0daefd88617f0e8
Jbachtiger <jbachtiger@gmail.com>
2022-07-18 13:18:36 +0000

    Add profile model, view and html template

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,15 @@
+# Profile Model Imports
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+# Profile Model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    profile_image = CloudinaryField('image', blank=True)
+    bio = models.TextField()
+
+    def __str__(self):
+        """ To return the user's username object as a string """
+        return f'Profile for {self.user.username}'
