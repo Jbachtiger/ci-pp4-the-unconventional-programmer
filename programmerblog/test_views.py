25eea19af065cd9a57ee4c936ef5e49bc477b2c0
Jbachtiger <jbachtiger@gmail.com>
2022-07-24 10:56:27 +0000

    Add a couple unit tests for views

---
+++ programmerblog/test_views.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+
+class TestViews(TestCase):
+    '''Class to perform various automated tests on the views'''
+
+    def test_home_page(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_create_post_page(self):
+        response = self.client.get("/create-post/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "create_post.html")
