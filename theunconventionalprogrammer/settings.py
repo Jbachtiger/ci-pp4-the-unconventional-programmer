f8a528ade3656b51775016c5de3891dc511dad2c
Jbachtiger <jbachtiger@gmail.com>
2022-07-24 13:32:41 +0000

    Update CKEDITOR_BASEPATH

--- theunconventionalprogrammer/settings.py
+++ theunconventionalprogrammer/settings.py
@@ -20,7 +20,7 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-CKEDITOR_BASEPATH = '/ckeditor/'
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 
 
 # Quick-start development settings - unsuitable for production
